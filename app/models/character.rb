8dbce1832b5b8f2e9e2526f3f0088203ef52670d
Igor German <igerman@chicagobooth.edu>
2016-11-15 00:29:48 -0600

    submission commit

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,7 +2,7 @@ class Character < ApplicationRecord
 
   belongs_to(:actor, :class_name => "Actor", :foreign_key => "actor_id")
 
-  belongs_to :movie
+  belongs_to  :movie
 
   validates :movie_id, :presence => true
   validates :actor_id, :presence => true
